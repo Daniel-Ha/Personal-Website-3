2b218da8e839c70f116766dfb931cd38e9f456a5
Daniel Ha <f003f9h@dartmouth.edu>
2024-04-10 18:01:25 -0700

    updating to dashboard

--- app/(main)/data_visualizations/page.tsx
+++ app/(main)/data_visualizations/page.tsx
@@ -2,7 +2,7 @@ const DataVisualizations = () => {
     return ( 
         <div className="flex flex-col items-center justify-center">
             <h1 className="pt-10 pb-10 text-3xl text-black dark:text-white">Data Visualizations</h1>
-            <iframe src="https://fenix-ai-partner.domo.com/embed/card/private/KZ75Y" width="1000" height="800"></iframe>
+            <iframe src="https://fenix-ai-partner.domo.com/embed/pages/private/32r3Q" width="900" height="800" ></iframe>
         </div>
      );
 }
